bd945988965e097ee6d6cc1f6b366f1b98eaaf38
vamsi krishna <srungarapu1989@gmail.com>
2014-03-01 09:01:39 -0800

    handled assigning unique id to the tweet divisions

--- file.js
+++ file.js
@@ -25,6 +25,16 @@ scr.innerText = "function getSuperParent(pare){"+
 "}";
 document.body.appendChild(scr);
 
+scr = document.createElement("script");
+scr.type="text/javascript";
+scr.innerText="function S4() {"+
+"   return (((1+Math.random())*0x10000)|0).toString(16).substring(1);"+
+"	 }"+
+"function guid() {"+
+"	   return (S4()+S4()+\"-\"+S4()+\"-\"+S4()+\"-\"+S4()+\"-\"+S4()+S4()+S4());"+
+"}";
+document.body.appendChild(scr);
+
 function getSuperParent(pare){
 	url = window.location.href;
 	groupsPattern=/.*\.facebook\.com\/groups\/.*/g;
@@ -88,7 +98,7 @@ d.addEventListener("DOMNodeInserted", function (ev) {
 	"if(typeof q.getElementsByClassName(\"userContent\")[0] == undefined){}"+
 	"else if(pare.className == \"UFICommentContentBlock\") {} else if(pare.getElementsByClassName(\"tweet\").length > 0){}"+
 	"else{mydiv = document.createElement(\"div\");"+
-	"mydiv.id=new Date().getTime();"+
+	"mydiv.id=guid();"+
 	"mydiv.className=\"tweet\";"+
 	"mydiv.setAttribute(\"onclick\",\"tweetFunction(this)\");"+
 	"mydiv.setAttribute(\"style\",\"position:relative;background-color:#F8F8F8;background-image:-webkit-gradient(linear,left top,left bottom,from(#FFF),to(#DEDEDE));background-image:-moz-linear-gradient(top,#FFF,#DEDEDE);background-image:-o-linear-gradient(top,#FFF,#DEDEDE);background-image:-ms-linear-gradient(top,#FFF,#DEDEDE);background-image:linear-gradient(top,#FFF,#DEDEDE);border:#CCC solid 1px;-moz-border-radius:3px;-webkit-border-radius:3px;border-radius:3px;color:#333;font-weight:bold;text-shadow:0 1px 0 rgba(255,255,255,.5);-webkit-user-select:none;-moz-user-select:none;-o-user-select:none;user-select:none;cursor:pointer;height:10px;max-width:98%;overflow:hidden;border-bottom: 1px solid #b3cee1;border-right: 1px solid #b3cee1;padding: 3px 4px 3px 4px;margin: 2px 2px 2px 0;text-decoration: none;font-size: 90%;font-style:italic;font-weight:bold;text-decoration-color: black;line-height: 1.4;white-space: nowrap;display: inline-block;float:right;cursor:pointer;\");"+
